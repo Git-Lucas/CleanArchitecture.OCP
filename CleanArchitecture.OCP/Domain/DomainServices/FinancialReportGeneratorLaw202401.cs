7b520a8f46deb37d364cc26d703cc80f268115aa
Lucas de Oliveira Gonçalves <lucas.oliver_2000@hotmail.com>
2024-07-24 23:21:44 -0300

    feat: Implementado método que gera um relatório financeiro com base em um conjunto de transações

--- CleanArchitecture.OCP/Domain/DomainServices/FinancialReportGeneratorLaw202401.cs
+++ CleanArchitecture.OCP/Domain/DomainServices/FinancialReportGeneratorLaw202401.cs
@@ -5,6 +5,22 @@ public class FinancialReportGeneratorLaw202401 : IFinancialReportGenerator
 {
     public FinancialReport Execute(IEnumerable<FinancialTransaction> financialTransactions)
     {
-        
+        decimal total = CalculateTotal(financialTransactions);
+
+        return new FinancialReport(financialTransactions, total);
+    }
+
+    private decimal CalculateTotal(IEnumerable<FinancialTransaction> financialTransactions)
+    {
+        decimal credits = financialTransactions
+                    .Where(x => x.Type == TypeFinancialTransaction.Credit)
+                    .Select(x => x.Value)
+                    .Sum();
+        decimal debits = financialTransactions
+            .Where(x => x.Type == TypeFinancialTransaction.Debit)
+            .Select(x => x.Value)
+            .Sum();
+
+        return credits - debits;
     }
 }
